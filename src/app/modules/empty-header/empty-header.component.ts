024cfbbd4de60238ff3ace376b774c5308991390
scottkang123 <48114611+scottkang123@users.noreply.github.com>
2024-05-08 20:54:54 -0500

    created diff headers and more pages:

---
+++ src/app/modules/empty-header/empty-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-header',
+  templateUrl: './empty-header.component.html',
+  styleUrl: './empty-header.component.css'
+})
+export class EmptyHeaderComponent {
+
+}
