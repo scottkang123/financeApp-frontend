44db96aeab62f9c0d3a1d2a21a673dfe2e5c3e52
parkj22 <jinyoung.park@vanderbilt.edu>
2024-05-26 00:26:55 +0900

    Implemented basic search bar that sends GET /api/v1/search/stock/{query} and logs result to console

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { SearchService } from '../search.service';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class SearchBarComponent {
+  query: string = "";
+
+  constructor(private searchService: SearchService) {
+
+  }
+
+  onSearch(): void {
+    this.searchService.searchStock(this.query).subscribe((data: any[]) => {
+      console.log(data);
+    }, (error) => {
+      console.error(error);
+    })
+  }
+}
