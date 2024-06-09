9ad5543fda15a51fc2e68abd4c0fff454d4b0cc1
scottkang123 <48114611+scottkang123@users.noreply.github.com>
2024-06-09 13:18:17 -0500

    search button and filter button

--- src/app/components/filter/finance-data-filter/filter-dialog/filter-dialog/filter-dialog.component.ts
+++ src/app/components/filter/finance-data-filter/filter-dialog/filter-dialog/filter-dialog.component.ts
@@ -1,14 +1,34 @@
+// filter-dialog.component.ts
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-filter-dialog',
   templateUrl: './filter-dialog.component.html',
-  styleUrl: './filter-dialog.component.css'
+  styleUrls: ['./filter-dialog.component.css']
 })
 export class FilterDialogComponent implements OnInit {
+  fields: string[] = [
+    'exchange', 'sector', 'industry', 'description', 'assetType', 'CIK', 'currency', 'country', 
+    'address', 'fiscalYearEnd', 'latestQuarter', 'marketCapitalization', 'EBITDA', 'PERatio', 
+    'PEGRatio', 'bookValue', 'dividendPerShare', 'dividendYield', 'EPS', 'revenuePerShareTTM', 
+    'profitMargin', 'operatingMarginTTM', 'returnOnAssetsTTM', 'returnOnEquityTTM', 'revenueTTM', 
+    'grossProfitTTM', 'dilutedEPSTTM', 'quarterlyEarningsGrowthYOY', 'quarterlyRevenueGrowthYOY', 
+    'analystTargetPrice', 'analystRatingStrongBuy', 'analystRatingBuy', 'analystRatingHold', 
+    'analystRatingSell', 'analystRatingStrongSell', 'trailingPE', 'forwardPE', 'priceToSalesRatioTTM', 
+    'priceToBookRatio', 'EVToRevenue', 'EVToEBITDA', 'beta', 'week52High', 'week52Low', 
+    'movingAverage50Day', 'movingAverage200Day', 'sharesOutstanding', 'dividendDate', 'exDividendDate'
+  ];
+  selectedFields: any = {};
+
+  constructor(private dialogRef: MatDialogRef<FilterDialogComponent>) {}
 
   ngOnInit(): void {
       
   }
 
+  applyFilters(): void {
+    // Pass selectedFields back to the component that opened the dialog
+    this.dialogRef.close(this.selectedFields);
+  }
 }
